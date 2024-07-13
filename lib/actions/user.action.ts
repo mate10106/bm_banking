7321ae8629db979f2f919583c200de3a85bf1ba4
Máté Brösztl <brosztl.mate@gmail.com>
2024-07-13 17:29:58 +0200

    Implement user authentication page UI with login and signup forms

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signUp = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
